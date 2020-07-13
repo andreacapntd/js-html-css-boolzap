82fa76f18c075605787c953f745ad3d323eb3b63
andreacapntd <andreacapntd@gmail.com>
2020-07-13 15:39:23 +0200

    add js cambio icona invio con intercettazione della tastiera

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log('hello');
+function showButtonSend() {
+  $('#txt-form').keydown(function(){
+    $('.send-btn .enter').removeClass('enter');
+    $('.send-btn .record').addClass('hide');
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function init() {
+  showButtonSend();
+};
+
+
+
+
+
+
+$(document).ready(init);
