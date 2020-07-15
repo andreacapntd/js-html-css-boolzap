98c4f46d175d4f0fda3e6afe03c5bd2c4aa2ec8e
Andrea Caprara <andreacaprara85@gmail.com>
2020-07-15 16:42:38 +0200

    prova di cancellazione messaggio

--- js/script.js
+++ js/script.js
@@ -1,61 +1,84 @@
 //funzione per mostrare il bottone di invio
 function showButtonSend() {
+
   $('#txt-form').keydown(function(){
     $('.send-btn .enter').removeClass('hide');
     $('.send-btn .record').addClass('hide');
   });
+
 }
+
 
 //funzione per invio mex utente
 function showUserMex() {
+
   $('#message-box .send-btn  img').click(function (){
+
     var template = $('#my-mex > div').clone();
     var target = $('.user-side.active');
     var userMex = $('#txt-form').val();
+
     template.find('#message-text').text(userMex);
     template.find('#mex-time').text(getActualHour());
+
     target.append(template);
+
     $('#txt-form').val('');
     $('.send-btn .record').removeClass('hide');
     $('.send-btn .enter').addClass('hide');
+
     showSpeakerMex();
   });
 }
 
 //funzione per invio messaggio interlocutore
 function showSpeakerMex() {
+
   setTimeout(function () {
+
     var template = $('#his-mex > div').clone();
     var target = $('.speaker-side.active');
+
     template.find('#mex-time').text(getActualHour());
     target.append(template);
 
   }, 1000);
-  // var speakerMex = $('#message-text-speaker');
+
 }
 
 //funzione per mostrare le opzioni del messaggio
 function showOptionMex() {
+
   $(document).on('click', '.fa-chevron-down', function() {
+
     $(this).find('.option').toggleClass('hide');
+
   });
+
 };
 
 //funzione per orario messaggio
 function getActualHour() {
+
   var date = new Date();
   return date.getHours() + ':' + date.getMinutes();
+
 }
 
 //funzione per ricerca contatti
 function showSearchContacts() {
+
   var target = $('#search-bar');
   target.keyup(searchKeyup);
+
 }
+
 function searchKeyup() {
+
   var input = $(this);
   var txt = input.val();
   var contacts = $('.contact');
+
   contacts.each(function() {
     var contact = $(this);
     var name = contact.find('.name').text();
@@ -65,6 +88,7 @@ function searchKeyup() {
       contact.hide();
     }
   })
+
 }
 
 //funzione per cambiare utente e chat
@@ -74,46 +98,55 @@ function clickUtente() {
   $(this).addClass('active');
 
   var contactDataValue = $(this).data('id');
-
-
   var userMex = $('.user-side');
   var speakerMex = $('.speaker-side');
 
   userMex.each(function() {
+
     var user = $(this);
     var userMexDataValue = $(user).data('id');
+
     if (userMexDataValue == contactDataValue) {
+
       user.addClass('active');
+
     } else {
+
       user.removeClass('active');
+
     }
+
   })
 
   speakerMex.each(function() {
+
     var speaker = $(this);
     var speakerMexDataValue = $(speaker).data('id');
+
     if (speakerMexDataValue == contactDataValue) {
+
       speaker.addClass('active');
 
     } else {
+
       speaker.removeClass('active');
+
     }
+
   })
+
 };
+
 function showContactChat() {
+
   $(document).on('click', '.contact', clickUtente);
-}
+
+};
 
 //funzione per cancellare il messaggio
-function deleteMex() {
-  $(document).on('click', '.mex-delete', function () {
-    var speakerMex = $('#his-mex');
-    var userMex = $('#my-mex');
-    $(this).remove(speakerMex);
-    $(this).remove(userMex);
 
-  });
-}
+
+
 
 
 
